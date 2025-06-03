1e0ecdd58b58e97463c66b5996df344ff5de0350
codizano <ruperto1@protonmail.com>
2025-06-03 15:49:00 -0500

    inter deleted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Fredoka } from "next/font/google";
+import { Fredoka } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 const fredoka = Fredoka({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
